70170ec35453b2392ba30138abaa59fa9fb4b293
zottwickel <scott.g.mckell@gmail.com>
2019-12-16 18:53:13 -0700

    use create-react-app to generate app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+       <h1>Hello UtahSlim!</h1>
+       <p>This app is (clearly) under construction.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
