358f2f22cceb0ef47bd7284e8b5523ed3d3264ae
Scott McKell <scott.g.mckell@gmail.com>
2019-12-17 17:21:26 -0700

    create footer

---
+++ src/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class Footer extends React.component {
+  
+  
+  
+  render() {
+    return (
+      <footer>
+        <p>This app was created by Scott McKell</p>
+        <p><a href="https://github.com/zottwickel">GitHub</a></p>
+        <p><a href="">LinkedIn</a></p>
+        <p><a href="">Portfolio</a></p>
+      </footer>
+    )
+  }
+}
+
+export default Footer
